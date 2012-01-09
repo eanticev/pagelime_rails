b0e2e9b786ba55d82cc1c94eaf79b4bb5c0a153b
Emil Anticevic <eanticev@gmail.com>
2012-01-09 13:08:51 -0800

    still trying to get routes set up

--- lib/engine.rb
+++ lib/engine.rb
@@ -2,7 +2,7 @@
 module Pagelime
   class Engine < Rails::Engine
     engine_name :pagelime
-    paths.config.routes = 'config/routes.rb'
+    # paths["config/routes"] << 'config/routes.rb'
     initializer "pagelime.initialize" do |app|
       initialize_pagelime_plugin
     end

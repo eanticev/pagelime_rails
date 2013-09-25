0e6e99284f3b8dfacca8d544cb2fe2bc6e1ef0a6
Joel Van Horn <joel@joelvanhorn.com>
2013-09-25 17:32:53 -0400

    Process editable area directly

--- lib/pagelime/rails/view_helper.rb
+++ lib/pagelime/rails/view_helper.rb
@@ -10,6 +10,11 @@ module Pagelime
         editable_content  = capture(&block)
         processed_html    = ::Pagelime.process_region(editable_content, request.path)
         
+        # tell pagelime-rack NOT to parse response body for whole page.
+        # this helper method automatically disables auto-processing, 
+        # so all editable areas on this page need to use this helper method.
+        ::Rack::Pagelime.disable_processing_for_request(request)
+        
         # output the final content
         concat(processed_html)
         

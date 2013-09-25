93e36933eb5ea815a70b63de31aafb53a635b3c0
Joel Van Horn <joel@joelvanhorn.com>
2013-09-25 17:33:26 -0400

    Removed Rails 2 classes in favor of using Rack with Rails 2.3+

--- lib/pagelime-rails.rb
+++ lib/pagelime-rails.rb
@@ -7,21 +7,11 @@ Pagelime.logger.debug "PAGELIME CMS RAILS PLUGIN: pagelime-rails gem loaded"
 # start plugin 
 if Rails::VERSION::MAJOR == 2
   
-  # TODO: Use pagelime-rack for Rails 2.3+ instead of routes and controllers
-  
-  require_relative 'pagelime/rails2/routing_extensions'
-  
-  ActionController::Routing::RouteSet::Mapper.send :include, ::Pagelime::Rails::RoutingExtensions
-  
-  controllers_path = File.expand_path File.join(File.dirname(__FILE__), 'pagelime', 'rails2', 'controllers')
-  
-  # add dependencies to load paths
-  $LOAD_PATH << controllers_path
-  load_paths << controllers_path
-  load_once_paths.delete(controllers_path)
-  
   Pagelime::Rails.initialize!
+  
+  ::Rails.application.config.middleware.use Rack::Pagelime
   
 elsif Rails::VERSION::MAJOR >= 3
   require_relative 'pagelime/rails/engine'
 end
+
--- lib/pagelime/rails2/routing_extensions.rb
+++
@@ -1,9 +0,0 @@
-module Pagelime #:nodoc:
-  module Rails2 #:nodoc:
-    module RoutingExtensions
-      def cms_routes
-        @set.add_route "/pagelime/:action", :controller => "pagelime_receiver"
-      end
-    end
-  end
-end
